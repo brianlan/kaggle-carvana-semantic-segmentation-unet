78885352a9d6ccb0a2860e6bdc66ed26b4d8aebd
brianlanbo <brianlanbo@gmail.com>
2017-08-20 22:51:15 +0800

    finished half of the data reading part

--- data_io.py
+++ data_io.py
@@ -18,14 +18,15 @@ class ImageFileName(str):
         return '{}.jpg'.format(self.fname)
 
     @property
-    def png(self):
-        return '{}.png'.format(self.fname)
+    def mask(self):
+        return '{}_mask.png'.format(self.fname)
 
 
-def read_images(data_dir, mask_dir=None, file_names=None, as_shape=(128, 128)):
+def read_images(data_dir, batch_size=16, as_shape=(128, 128), mask_dir=None, file_names=None):
     """
     If file_names not provided,
     :param data_dir: dir of the data to be read
+    :param batch_size: size of a batch
     :param file_names: if file_names not provided, all the data in data_dir will be read
     :param mask_dir: if mask_dir is provided, not only the data, but also the mask (label) will be read
     :param as_shape: will execute image reshape according to provided as_shape param.
@@ -35,9 +36,24 @@ def read_images(data_dir, mask_dir=None, file_names=None, as_shape=(128, 128)):
     def _c(*args):
         return os.path.join(*args)
 
-    for f in file_names:
-        img_path = _c(data_dir, f.jpg)
-        img = scipy.misc.imread(img_path)
-        img = scipy.misc.imresize(img, as_shape)
-        pass
+    def _append_img(image_list, data_dir, fname, shape):
+        path = _c(data_dir, fname)
+        img = scipy.misc.imread(path)
+        img = scipy.misc.imresize(img, shape)
+        image_list.append(img)
 
+    file_names = file_names or [ImageFileName(f.split('.')[0]) for f in os.listdir(data_dir)]
+    while True:
+        for start in range(0, len(file_names), batch_size):
+            img_batch = []
+            mask_batch = []
+            end = min(start + batch_size, len(file_names))
+            batch_fnames = file_names[start:end]
+
+            for f in batch_fnames:
+                _append_img(img_batch, data_dir, f.jpg, as_shape)
+
+                if mask_dir:
+                    _append_img(mask_batch, mask_dir, f.mask, as_shape)
+
+            yield img_batch, mask_batch if mask_dir else None
--- train.py
+++ train.py
@@ -10,6 +10,7 @@ from data_io import read_images, ImageFileName
 df = pd.read_csv(os.path.join(INPUT_DIR, 'train_masks.csv'))
 fnames = [ImageFileName(f.split('.')[0]) for f in df['img'].tolist()]
 fnames_train, fnames_validation = train_test_split(fnames, test_size=0.2, random_state=233)
-train_data = read_images(TRAIN_DATA_DIR, mask_dir=TRAIN_MASK_DIR, file_names=fnames_train)
+train_data = read_images(TRAIN_DATA_DIR, batch_size=16, as_shape=(128, 128), mask_dir=TRAIN_MASK_DIR,
+                         file_names=fnames_train)
 
 pass
