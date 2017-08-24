acb8b1822e1b3f51a01bea42500158d3e8848f80
brianlanbo <brianlanbo@gmail.com>
2017-08-24 23:18:00 +0800

    added max_pooling_layer and up_sampling_layer to the helpers.py and also implemented downward / center / upward part of unet by calling the defined layers

--- model/helpers.py
+++ model/helpers.py
@@ -17,8 +17,8 @@ def bias_variable(shape):
     return tf.Variable(initial)
 
 
-def batchnorm_layer(X, is_test, offset, scale, iteration, convolutional=False):
-    exp_moving_avg = tf.train.ExponentialMovingAverage(0.9999, iteration)
+def batchnorm_layer(X, is_test, offset, scale, convolutional=False):
+    exp_moving_avg = tf.train.ExponentialMovingAverage(0.9999)
     if convolutional:
         mean, variance = tf.nn.moments(X, [0, 1, 2])
     else:
@@ -31,20 +31,46 @@ def batchnorm_layer(X, is_test, offset, scale, iteration, convolutional=False):
     return bn, update_moving_averages
 
 
-def conv_layer(X, filter_shape, name):
+def conv_layer(X, filter_shape, stride=1, is_test=False, use_bn=False, name=None):
     """
     :param X: the input data, which is of shape (N, H, W, C)
     :param filter_shape: the shape of filter, which is of shape (H, W, C)
+    :param stride:
+    :param is_test: as the var name presents
+    :param use_bn: boolean param that indicates whether the conv_layer uses batchnorm whithin it
     :param name: tf's variable scope name
     :return:
     """
     with tf.variable_scope(name) as scope:
         filt = weight_variable(filter_shape)
-        conv = tf.nn.conv2d(X, filt, [1, 1, 1, 1], padding='SAME')
+        conv = tf.nn.conv2d(X, filt, [1, stride, stride, 1], padding='SAME')
         bias = bias_variable(filter_shape[2])
-        bn = batchnorm_layer(conv + bias)
-        relu = tf.nn.relu(bn)
+        logits = conv + bias
+
+        if use_bn:
+            scale = tf.Variable(tf.float32, shape=(X.shape[3]))  # aka. alpha
+            offset = tf.Variable(tf.float32, shape=(X.shape[3]))  # aka. beta
+            bn = batchnorm_layer(logits, is_test, offset, scale, convolutional=True)
+
+        relu = tf.nn.relu(bn if use_bn else logits)
 
         # Add summary to Tensorboard
-        activation_summary(relu)
+        # activation_summary(relu)
         return relu
+
+
+def max_pooling_layer(X, kernel_size=2, stride=2, padding='SAME', name=None):
+    """input data should be of shape (N, H, W, C)"""
+    with tf.variable_scope(name) as scope:
+        pool = tf.nn.max_pool(X, ksize=[1, kernel_size, kernel_size, 1],
+                              strides=[1, stride, stride, 1],
+                              padding=padding, name=name)
+    return pool
+
+
+def up_sampling_layer(X, filter_shape, output_shape, stride=2, padding='SAME', name=None):
+    with tf.variable_scope(name):
+        filt = weight_variable(filter_shape)
+        deconv = tf.nn.conv2d_transpose(X, filt, output_shape, strides=[1, stride, stride, 1], padding=padding)
+
+    return deconv
--- model/unet.py
+++ model/unet.py
@@ -1,16 +1,68 @@
 import tensorflow as tf
 
 
-from .helpers import conv_layer
+from .helpers import conv_layer, max_pooling_layer, up_sampling_layer
+
+
+class NotSupportedUNetResolution(Exception):
+    pass
 
 
 class UNet:
+    lookup_table = {128: 64, 256: 32, 512: 16, 1024: 8}
+
     def __init__(self, num_classes, input_shape=128, tf_scope='unet'):
+        if input_shape not in [128, 256, 512, 1024]:
+            raise NotSupportedUNetResolution('Only 128, 256, 512 and 1024 are valid shape for UNet, but {!r} is given.'.format(input_shape))
+
         self.tf_scope = tf_scope
         self.num_classes = num_classes
         self.input_shape = (input_shape, input_shape)
+        self.start_num_filters = self.lookup_table[input_shape]
+        self.params = {
+            'down': [],
+            'center': [],
+            'up': []
+        }
+
+    def _build_downward_layer(self, feat, num_filters, d='down'):
+        f_shape = (3, 3, num_filters)
+        basename = d + num_filters
+        self.params[basename+'a'] = conv_layer(feat, f_shape, 1, False, use_bn=True, name=basename+'a')
+        self.params[basename+'b'] = conv_layer(self.params[d][-1], f_shape, 1, False, use_bn=True, name=basename+'b')
+        self.params[basename+'pool'] = max_pooling_layer(self.params[d][-1], 2, 2, name=basename+'pool')
+        return self.params[basename+'pool']
+
+    def _build_upward_layer(self, feat, num_filters, d='up'):
+        up_filter_shape = (2, 2, num_filters)
+        basename = d + num_filters
+        self.params[basename+'deconv'] = up_sampling_layer(feat, up_filter_shape, (feat.shape[1], feat.shape[2]), stride=2, name=basename+'deconv')
+        merged = tf.concat([self.param['down'+num_filters+'b'], self.params[basename+'deconv']], axis=3)
+        self.params[basename+'a'] = conv_layer(merged, (3, 3, num_filters), 1, False, use_bn=True, name=basename+'a')
+        self.params[basename+'b'] = conv_layer(self.params[basename+'a'], (3, 3, num_filters), 1, False, use_bn=True, name=basename+'b')
+        return self.params[basename+'b']
 
     def build(self):
         with tf.name_scope(self.tf_scope):
-            X_train = tf.placeholder(tf.float32, shape=(self.input_shape, self.input_shape, 3), name='X_train')
-            self.conv1 = conv_layer(X_train, (3, 3, 64))
+            feat = tf.placeholder(tf.float32, shape=(self.input_shape, self.input_shape, 3), name='X_train')
+            num_filters = self.start_num_filters
+
+            #################
+            #   downward
+            #################
+            while num_filters < 1024:
+                feat = self._build_downward_layer(feat, num_filters)
+                num_filters *= 2
+
+            #################
+            #   center
+            #################
+            feat = self._build_downward_layer(feat, num_filters, d='center')
+            num_filters /= 2
+
+            #################
+            #   upward
+            #################
+            while num_filters >= self.start_num_filters:
+                feat = self._build_upward_layer(feat, num_filters)
+                num_filters /= 2
