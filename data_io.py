fixed the problem of cross_entropy loss is nan by normalizing y_train when reading the image.

data_io.py
@@ -37,10 +37,14 @@ def read_images(data_dir, batch_size=16, as_shape=128, mask_dir=None, file_names
     def _c(*args):
         return os.path.join(*args)
 
-    def _read_img(data_dir, fname, shape):
+    def _read_img(data_dir, fname, shape, normalize=False, black_or_white=False):
         path = _c(data_dir, fname)
         img = scipy.misc.imread(path)
         img = scipy.misc.imresize(img, (shape, shape))
+
+        if normalize:
+            img = img // 255 if black_or_white else img / 255
+
         return img
 
     file_names = file_names or [ImageFileName(f.split('.')[0]) for f in os.listdir(data_dir)]
@@ -55,6 +59,7 @@ def read_images(data_dir, batch_size=16, as_shape=128, mask_dir=None, file_names
                 img_batch.append(_read_img(data_dir, f.jpg, as_shape))
 
                 if mask_dir:
-                    mask_batch.append(np.expand_dims(_read_img(mask_dir, f.mask, as_shape), axis=2))
+                    im = _read_img(mask_dir, f.mask, as_shape, normalize=True, black_or_white=True)
+                    mask_batch.append(np.expand_dims(im, axis=2))
 
             yield np.array(img_batch, np.float32), np.array(mask_batch, np.float32) if mask_dir else None

model/unet.py
@@ -25,7 +25,7 @@ class UNet:
         self.y_train = tf.placeholder(tf.int32, shape=(None, self.input_shape, self.input_shape, 1), name='y_train')
         self.global_step = tf.Variable(0, name='global_step', trainable=False)
         self.params = {}
-        self.pred = self.cross_entropy = self.optimizer = self.train_op = None
+        self.pred = self.loss = self.optimizer = self.train_op = None
 
     def _build_downward_layer(self, feat, num_filters, d='down'):
         f_shape = (3, 3, num_filters)
@@ -87,10 +87,10 @@ class UNet:
             #   classification
             #####################
             self.pred = conv_layer(feat, (1, 1, self.num_classes), name='classifier')
-            self.flat_y_train = tf.reshape(self.y_train, [-1])
-            self.flat_pred = tf.reshape(self.pred, [-1, self.num_classes])
-            self.cross_entropy = tf.reduce_mean(
+            # self.flat_y_train = tf.reshape(self.y_train, [-1])
+            # self.flat_pred = tf.reshape(self.pred, [-1, self.num_classes])
+            self.loss = tf.reduce_mean(
                 tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tf.reshape(self.y_train, [-1]),
                                                                logits=tf.reshape(self.pred, [-1, self.num_classes])))
             self.optimizer = tf.train.RMSPropOptimizer(learning_rate=self.learning_rate, decay=0.9)
-            self.train_op = self.optimizer.minimize(self.cross_entropy, global_step=self.global_step)
+            self.train_op = self.optimizer.minimize(self.loss, global_step=self.global_step)

train.py
@@ -13,6 +13,7 @@ from data_io import read_images, ImageFileName
 #  Global Parameters Definition
 ######################################
 PROJECT_HOME = '/home/rlan/projects/Kaggle/Carnava/kaggle-carvana-semantic-segmentation-unet'
+CHECKPOINT_DIR = os.path.join(PROJECT_HOME, 'checkpoints')
 INPUT_DIR = os.path.join(PROJECT_HOME, 'input')
 TRAIN_DATA_DIR = os.path.join(INPUT_DIR, 'train')
 TRAIN_MASK_DIR = os.path.join(INPUT_DIR, 'train_masks')
@@ -40,15 +41,17 @@ with tf.Session() as sess:
     unet.build()
     init = tf.global_variables_initializer()
     sess.run(init)
+    saver = tf.train.Saver(tf.global_variables(), max_to_keep=5)
 
     for epoch in range(EPOCHS):
         start_time = time.time()
         for batch, (X_batch, y_batch) in enumerate(train_data):
-            _, loss, pred = sess.run([unet.train_op, unet.cross_entropy, unet.pred],
+            _, loss, pred = sess.run([unet.train_op, unet.loss, unet.pred],
                                      feed_dict={unet.X_train: X_batch, unet.y_train: y_batch})
 
             print('[epoch {}, batch {}] cross_entropy: {}'.format(epoch, batch, loss))
 
         print('epoch {} took {:.0f} seconds to train.'.format(epoch, time.time() - start_time))
 
+    saver.save(sess, CHECKPOINT_DIR, global_step=epoch)
     pass