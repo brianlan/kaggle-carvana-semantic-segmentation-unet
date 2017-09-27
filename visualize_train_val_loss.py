9bb379c7408d34147613aefdf24f8295f5c53329
brianlanbo <brianlanbo@gmail.com>
2017-09-27 14:04:42 +0800

    added argparse to visualize_train_val_loss

--- visualize_train_val_loss.py
+++ visualize_train_val_loss.py
@@ -1,12 +1,17 @@
 import subprocess
+import argparse
 
 import pandas as pd
 
 
-log_path = "/home/rlan/projects/Kaggle/Carnava/kaggle-carvana-semantic-segmentation-unet/log/20170915"
+parser = argparse.ArgumentParser(description='visualize the train / val losses')
+parser.add_argument('--log-path', type=str, required=True, help='the path to the log')
+args = parser.parse_args()
+
+log_path = args.log_path  #"/home/rlan/projects/Kaggle/Carnava/kaggle-carvana-semantic-segmentation-unet/log/20170915"
 output_path = "/tmp/train_val_error.csv"
 
-train_output = subprocess.check_output("cat {} | grep 'batch 254] training loss' | awk '{{print $14}}'".format(log_path), shell=True)
+train_output = subprocess.check_output("cat {} | grep 'batch 508] training loss' | awk '{{print $16}}'".format(log_path), shell=True)
 val_output = subprocess.check_output("cat {} | grep 'validation loss' | awk '{{print $12}}'".format(log_path), shell=True)
 train_err = train_output.strip().split(b'\n')
 val_err = val_output.strip().split(b'\n')
